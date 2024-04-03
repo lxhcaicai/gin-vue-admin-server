932a1126a25476d46ffccc2d1ac7ffe2841be049
lxhcaicai <2778763221@qq.com>
2024-04-04 00:56:54 +0800

    搭建基础 自动初始化表

--- initialize/gorm.go
+++ initialize/gorm.go
@@ -2,7 +2,10 @@ package initialize
 
 import (
 	"github.com/lxhcaicai/gin-vue-admin/server/global"
+	"github.com/lxhcaicai/gin-vue-admin/server/model/system"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
+	"os"
 )
 
 func Gorm() *gorm.DB {
@@ -12,4 +15,16 @@ func Gorm() *gorm.DB {
 	default:
 		return GormMysql()
 	}
+}
+
+func RegisterTables() {
+	db := global.GVA_DB
+	err := db.AutoMigrate(
+		system.SysApi{},
+	)
+	if err != nil {
+		global.GVA_LOG.Error("register table failed", zap.Error(err))
+		os.Exit(0)
+	}
+	global.GVA_LOG.Info("register table success")
 }
--- main.go
+++ main.go
@@ -15,5 +15,11 @@ func main() {
 	global.GVA_DB = initialize.Gorm() //gorm连接数据库
 	initialize.Timer()                //定时任务
 	initialize.DBList()
+	if global.GVA_DB != nil {
+		initialize.RegisterTables() // 初始化表
+		// 程序结束前关闭数据库链接
+		db, _ := global.GVA_DB.DB()
+		defer db.Close()
+	}
 	core.RunWindowsServer()
 }
